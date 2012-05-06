95c18c5acccf7b2668be50f38bce093c2271bb55
bertin <bertin@9b2bc9b4-f3a3-41e6-8a4a-3080b552da83>
2012-05-06 22:55:59 +0000

    Fixed compilation issue with FFTW library. Pushed version number to 3.8.3.

--- trunk/src/globals.h
+++ trunk/src/globals.h
@@ -7,7 +7,7 @@
 *
 *	This file part of:	SkyMaker
 *
-*	Copyright:		(C) 1998-2010 Emmanuel Bertin -- IAP/CNRS/UPMC
+*	Copyright:		(C) 1998-2012 Emmanuel Bertin -- IAP/CNRS/UPMC
 *
 *	License:		GNU General Public License
 *
@@ -22,13 +22,14 @@
 *	You should have received a copy of the GNU General Public License
 *	along with SkyMaker. If not, see <http://www.gnu.org/licenses/>.
 *
-*	Last modified:		12/10/2010
+*	Last modified:		06/01/2012
 *
 *%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
 
 #include "types.h"
 
 /*----------------------- miscellaneous variables ---------------------------*/
+char		gstr[MAXCHAR];
 
 /*------------------------------- functions ---------------------------------*/
 extern void	makeit(void);
