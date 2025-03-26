053d7cc5704c6b47d2c32ef4af9e339848b1487b
Emmanuel Bertin <bertin@iap.fr>
2025-03-27 00:13:08 +0100

    Update PThread autoconfig.

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -81,7 +81,7 @@ language = None
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
 #
-today = 'Mon Sep 27 2021'
+today = 'Thu Mar 27 2025'
 #
 # Else, today_fmt is used as the format for a strftime call.
 #
